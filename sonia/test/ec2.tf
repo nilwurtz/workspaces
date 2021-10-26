21a5ddf34f1e3f327471be2dfebe07000ff56acd
ragnar1904 <apollo.oth@gmail.com>
2021-10-27 00:53:10 +0900

    use latest ami and use userdata

--- sonia/test/ec2.tf
+++ sonia/test/ec2.tf
@@ -1,6 +1,14 @@
 resource "aws_instance" "sample_ubuntu" {
-  ami                    = "ami-036d0684fc96830ca"
+  ami                    = data.aws_ami.ubuntu_20_04.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.test_subnet.id
   vpc_security_group_ids = [aws_security_group.test_sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+apt update
+apt install -y language-pack-ja-base language-pack-ja ibus-mozc
+localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+source /etc/default/locale
+EOF
 }
