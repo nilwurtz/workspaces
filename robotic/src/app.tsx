f2e8b8495dd48c32c394191ea7557a02d3acdef9
ragnar1904 <apollo.oth@gmail.com>
2022-03-05 22:19:00 +0900

    vite preact project

---
+++ robotic/src/app.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'preact'
+
+export const App: FunctionComponent = () => {
+  return (
+    <main >
+      <section>
+        <div className='container p-4 bg-teal-400 drop-shadow-md'>
+          <h1 className='text-white text-left'>Sample App</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
